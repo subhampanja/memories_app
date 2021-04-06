fbb5893a8287edd078eaa15153b38c8b0fbb7067
subhampanja28 <subhampanja28@gmail.com>
2021-04-06 23:24:26 +0530

    some updates

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,22 @@
-export const getPosts =  (req,res) => {
-    res.send("I am post get")
+import PostMessage from "../models/postMessage.js"
+
+export const getPosts = async(req, res) => {
+    try {
+        const postMessage = await PostMessage.find();
+
+        res.status(200).json(postMessage);
+    } catch (error) {
+        res.status(404).json({ mesage: error.message });
+    }
+}
+export const createPost = async(req, res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post);
+    try {
+        await newPost.save();
+
+        res.status(200).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
